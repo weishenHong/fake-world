e231ca971bf3bf56854ec52237852b0227d63414
weishen-hong <57124685+weishenHong@users.noreply.github.com>
2024-05-07 09:01:19 +0000

    11111111111111111111111111111111111111111111

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,22 +1,27 @@
-import { App as AntdApp, ConfigProvider } from 'antd';
+import { App as AntdApp, ConfigProvider,FloatButton } from 'antd';
 import { useTranslation } from 'react-i18next';
-
 import LeftPanel from './components/LeftPanel';
 import RightPanel from './components/RightPanel';
 import Screen from './components/Screen';
 import TopPopover from './components/TopPopover';
 import Tour from './components/Tour';
 import { ANTD_LANG_MAP } from './i18n';
+import { useState } from 'react';
 
 const App = () => {
   const inShareMode = !!window.__SHARE_KEY__;
   const { i18n } = useTranslation();
-
+  const [dev, setDev] = useState(false)
   return (
     <ConfigProvider locale={ANTD_LANG_MAP[i18n.language as keyof typeof ANTD_LANG_MAP]}>
+      <FloatButton  description="Dev" shape="square" onClick={() => 
+        {
+          console.log(!dev);
+          setDev(!dev)
+        } } />
       <div className="grid min-h-screen grid-cols-3 max-lg:grid-cols-1">
-        {!inShareMode && (
-          <AntdApp className="max-lg:hidden">
+        {dev && (
+          <AntdApp >
             <LeftPanel />
           </AntdApp>
         )}
@@ -30,7 +35,7 @@ const App = () => {
             </TopPopover>
           </div>
         </div>
-        {!inShareMode && <RightPanel />}
+        { dev && <RightPanel />}
       </div>
       <Tour />
     </ConfigProvider>
--- packages/web/src/components/RightPanel/index.tsx
+++ packages/web/src/components/RightPanel/index.tsx
@@ -4,7 +4,7 @@ import { MetaDataEditor } from '../MetaDataEditor';
 
 const RightPanel = () => {
   return (
-    <div className="flex h-screen flex-col max-lg:hidden">
+    <div className="flex h-screen flex-col ">
       <div className="flex-1 overflow-auto border-y border-dashed border-orange-400 p-4">
         <div>
           <MetaDataEditor />
